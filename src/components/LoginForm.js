be44f056f589383b7a3c350798f91de69252077a
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-10-27 08:25:47 +0300

    casing fixed

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,21 +3,6 @@ import { Form, Input, Button, Checkbox, Row, Col } from "antd";
 import { connect } from "react-redux";
 import { SignIn } from "../redux/actions/AuthActions";
 import { Redirect } from "react-router-dom";
-//import { Redirect } from "react-router-dom";
-
-// should we move state management to parent and keep this one as presentational?
-
-//before reading the explanation below, read all the comments within the code.
-//combined reducers are inside the store.
-//when user fills the form and clicks on log in line 44 gets invoked.
-//line 49 triggers line 18 .
-//at line 18 redux dispatches logIn("LOG_IN" action from actions folder) as action to store.
-//store finds LOG_IN from combinedReducers and calls isLoggedIn reducer(inside reducer folder).
-//that reducer creates new state and sends it back to store.
-//State gets updated.
-//Thus components get re-rendered.
-
-// test
 
 function LoginForm(props) {
     const layout = {
@@ -28,17 +13,16 @@ function LoginForm(props) {
         wrapperCol: { offset: 8, span: 16 },
     };
 
-    //this is like onSubmit for some reason ant Design uses onFinish
     const onFinish = values => {
         console.log("Success:", values);
-        props.auth(values); // changes state . this was defined in line 18
+        props.auth(values);
     };
 
     const onFinishFailed = errorInfo => {
         console.log("Failed:", errorInfo);
     };
     if (props.authStatus) {
-        return <Redirect to="/diet" />;
+        return <Redirect to="/" />;
     } else {
         return (
             <Row style={{ marginTop: "10vh" }}>
@@ -102,9 +86,6 @@ function LoginForm(props) {
 }
 
 const mapStateToProps = state => {
-    //makes the state accessible as props(isLogged will be a props for the functional component above)
-    // console.log("state is", state)
-    // console.log("auth uid is", state.firebase.auth.uid)
     return {
         authError: state.AuthReducer.authError,
         authStatus: state.firebase.auth.uid,
@@ -112,10 +93,9 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => {
-    //allows you to change state
     return {
-        auth: user => dispatch(SignIn(user)), // logIn refers to a type of action(check actions folder).   must be a function. dispatches action type, redux executes it accordingly
+        auth: user => dispatch(SignIn(user)),
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginForm); //connects redux store to component. parameters allows you to see and change the state.
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
