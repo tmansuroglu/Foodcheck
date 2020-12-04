a2be550b671dd118dce64fc82adc6c6a9952350d
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-12-04 18:09:26 +0300

    daily stats configured

--- src/components/dietpage/DailyStats/index.jsx
+++ src/components/dietpage/DailyStats/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { List } from 'antd';
+import './index.css';
 
 const DailyStats = ({ nutrientsConsumed }) => {
-  console.log(nutrientsConsumed);
-  return <div>daily stats</div>;
+  const data = [
+    `Fat: ${nutrientsConsumed.total_fat} gr`,
+    `Carbohydrate : ${nutrientsConsumed.total_carbohydrate} gr`,
+    `Protein: ${nutrientsConsumed.protein} gr`,
+    `Sugar: ${nutrientsConsumed.sugars} gr`,
+    `Sodium: ${nutrientsConsumed.sodium} mg`,
+    `Potassium: ${nutrientsConsumed.potassium} mg`,
+    `Cholesterol: ${nutrientsConsumed.cholesterol} mg`,
+    `Fibers: ${nutrientsConsumed.fibers} gr`,
+  ];
+  return (
+    <List
+      size='small'
+      dataSource={data}
+      renderItem={item => <List.Item>{item}</List.Item>}
+    />
+  );
 };
 
 export default DailyStats;
