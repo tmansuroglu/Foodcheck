2fed415cfb35cdedf8568fbb04bf88af2b8f0337
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-12-04 19:15:26 +0300

    delete old backgrounds, replace diet page background with a small size pattern

--- src/components/dietpage/EditMeal/index.jsx
+++ src/components/dietpage/EditMeal/index.jsx
@@ -10,137 +10,91 @@ import Sort from '../SortFood';
 import './index.css';
 
 const EditMeal = ({ activeMeal }) => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [editTarget, setEditTarget] = useState();
-    const [activeMealName, setActiveMealName] = useState('');
-    const [activeMealContent, setActiveMealContent] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editTarget, setEditTarget] = useState();
+  const [activeMealName, setActiveMealName] = useState('');
+  const [activeMealContent, setActiveMealContent] = useState([]);
 
-    useEffect(() => {
-        if (activeMeal) {
-            setActiveMealName(Object.keys(activeMeal)[0]);
-            setActiveMealContent(Object.values(activeMeal)[0]);
-        }
-    }, [activeMeal]);
+  useEffect(() => {
+    if (activeMeal) {
+      setActiveMealName(Object.keys(activeMeal)[0]);
+      setActiveMealContent(Object.values(activeMeal)[0]);
+    }
+  }, [activeMeal]);
 
-    const handleEditButton = food => {
-        setIsEditing(!isEditing);
-        setEditTarget(food);
-    };
+  const handleEditButton = food => {
+    setIsEditing(!isEditing);
+    setEditTarget(food);
+  };
 
-    return (
-        <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
-            <Row justify='center'>
-                {activeMealName && activeMealContent ? (
-                    <>
-                        <Card
-                            className='editMealCard'
-                            hoverable='true'
-                            title={
-                                <>
-                                    <SearchBar />
-                                    <Sort />
-                                </>
-                            }
+  return (
+    <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
+      <Row justify='center'>
+        {activeMealName && activeMealContent ? (
+          <>
+            <Card
+              className='editMealCard'
+              title={
+                <>
+                  <SearchBar />
+                  <Sort />
+                </>
+              }
+            >
+              <Row>
+                {activeMealContent.map((food, index) => {
+                  return (
+                    <Col key={index} xs={24} className='editMealCol'>
+                      <List itemLayout='horizontal'>
+                        <List.Item
+                          actions={[
+                            <Button onClick={e => handleEditButton(food)}>
+                              edit
+                            </Button>,
+                            <DeleteFood food={food} />,
+                          ]}
                         >
-                            <Row>
-                                {activeMealContent.map((food, index) => {
-                                    return (
-                                        <Col
-                                            key={index}
-                                            xs={24}
-                                            className='editMealCol'
-                                        >
-                                            <List itemLayout='horizontal'>
-                                                <List.Item
-                                                    actions={[
-                                                        <Button
-                                                            onClick={e =>
-                                                                handleEditButton(
-                                                                    food
-                                                                )
-                                                            }
-                                                        >
-                                                            edit
-                                                        </Button>,
-                                                        <DeleteFood
-                                                            food={food}
-                                                        />,
-                                                    ]}
-                                                >
-                                                    <List.Item.Meta
-                                                        avatar={
-                                                            <Avatar
-                                                                src={
-                                                                    food.photo
-                                                                        .thumb
-                                                                }
-                                                            />
-                                                        }
-                                                        title={
-                                                            isEditing ? (
-                                                                <EditFood
-                                                                    food={food}
-                                                                    isEditing={
-                                                                        isEditing
-                                                                    }
-                                                                    setIsEditing={
-                                                                        setIsEditing
-                                                                    }
-                                                                    editTarget={
-                                                                        editTarget
-                                                                    }
-                                                                />
-                                                            ) : (
-                                                                <>
-                                                                    {
-                                                                        food
-                                                                            .nutrientsConsumed
-                                                                            .serving_amount
-                                                                    }{' '}
-                                                                    {
-                                                                        food
-                                                                            .nutrientsConsumed
-                                                                            .serving_size
-                                                                    }{' '}
-                                                                    {
-                                                                        food.food_name
-                                                                    }{' '}
-                                                                    <CaretRightOutlined />{' '}
-                                                                    {
-                                                                        food
-                                                                            .nutrientsConsumed
-                                                                            .calories
-                                                                    }{' '}
-                                                                    kcal
-                                                                </>
-                                                            )
-                                                        }
-                                                        description={
-                                                            <Description
-                                                                food={food}
-                                                            />
-                                                        }
-                                                    />
-                                                </List.Item>
-                                            </List>
-                                        </Col>
-                                    );
-                                })}
-                            </Row>
-                        </Card>
-                    </>
-                ) : (
-                    ''
-                )}
-            </Row>
-        </Col>
-    );
+                          <List.Item.Meta
+                            avatar={<Avatar src={food.photo.thumb} />}
+                            title={
+                              isEditing ? (
+                                <EditFood
+                                  food={food}
+                                  isEditing={isEditing}
+                                  setIsEditing={setIsEditing}
+                                  editTarget={editTarget}
+                                />
+                              ) : (
+                                <>
+                                  {food.nutrientsConsumed.serving_amount}{' '}
+                                  {food.nutrientsConsumed.serving_size}{' '}
+                                  {food.food_name} <CaretRightOutlined />{' '}
+                                  {food.nutrientsConsumed.calories} kcal
+                                </>
+                              )
+                            }
+                            description={<Description food={food} />}
+                          />
+                        </List.Item>
+                      </List>
+                    </Col>
+                  );
+                })}
+              </Row>
+            </Card>
+          </>
+        ) : (
+          ''
+        )}
+      </Row>
+    </Col>
+  );
 };
 
 const mapStateToProps = state => {
-    return {
-        activeMeal: state.DietReducer.activeMeal,
-    };
+  return {
+    activeMeal: state.DietReducer.activeMeal,
+  };
 };
 
 export default connect(mapStateToProps)(EditMeal);
--- src/container/AboutPage/index.jsx
+++ src/container/AboutPage/index.jsx
@@ -3,33 +3,31 @@ import { Typography, Row, Col } from 'antd';
 import './index.css';
 
 const AboutPage = () => {
-    const { Title } = Typography;
+  const { Title } = Typography;
 
-    return (
-        <Row className='aboutPageRow'>
-            <Col span={20} offset={2} className='aboutPageCol'>
-                <Title level={1}> About</Title>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                    venenatis diam in eros eleifend, a tristique ex tempus. Nam
-                    at elit odio. Nulla vehicula, neque a venenatis scelerisque,
-                    massa ante luctus ante, nec sollicitudin justo velit ut
-                    augue. Sed faucibus nunc non bibendum fermentum. Morbi sed
-                    lorem vel quam fringilla dictum. Vestibulum vestibulum
-                    accumsan mauris, non sollicitudin lacus egestas laoreet.
-                    Quisque porta sagittis purus, sed venenatis eros
-                    sollicitudin id. Integer non tellus orci. Maecenas vulputate
-                    ligula sit amet nunc cursus, nec viverra nibh ornare. Morbi
-                    dolor nunc, venenatis vitae eros vel, laoreet egestas
-                    ligula. Etiam suscipit lorem eget felis mollis, in auctor
-                    magna placerat. Morbi semper libero nulla, vitae euismod
-                    augue vulputate id. Maecenas molestie nibh at nulla
-                    pharetra, at rhoncus nunc fringilla. Phasellus ac purus in
-                    turpis laoreet maximus a vel dolor.
-                </p>
-            </Col>
-        </Row>
-    );
+  return (
+    <Row className='aboutPageRow'>
+      <Col span={20} offset={2} className='aboutPageCol'>
+        <Title level={1}> About</Title>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis
+          diam in eros eleifend, a tristique ex tempus. Nam at elit odio. Nulla
+          vehicula, neque a venenatis scelerisque, massa ante luctus ante, nec
+          sollicitudin justo velit ut augue. Sed faucibus nunc non bibendum
+          fermentum. Morbi sed lorem vel quam fringilla dictum. Vestibulum
+          vestibulum accumsan mauris, non sollicitudin lacus egestas laoreet.
+          Quisque porta sagittis purus, sed venenatis eros sollicitudin id.
+          Integer non tellus orci. Maecenas vulputate ligula sit amet nunc
+          cursus, nec viverra nibh ornare. Morbi dolor nunc, venenatis vitae
+          eros vel, laoreet egestas ligula. Etiam suscipit lorem eget felis
+          mollis, in auctor magna placerat. Morbi semper libero nulla, vitae
+          euismod augue vulputate id. Maecenas molestie nibh at nulla pharetra,
+          at rhoncus nunc fringilla. Phasellus ac purus in turpis laoreet
+          maximus a vel dolor.
+        </p>
+      </Col>
+    </Row>
+  );
 };
 
 export default AboutPage;
