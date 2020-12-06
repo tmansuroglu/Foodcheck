b321afefe712a6043f7c12b0f4b46b973d009440
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-12-06 18:13:14 +0300

    delete comments

--- src/container/LoginPage/index.jsx
+++ src/container/LoginPage/index.jsx
@@ -8,10 +8,7 @@ import db from '../../firebaseConfig';
 import { register } from '../../redux/actions/AuthActions';
 
 const UI_CONFIG = {
-  // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
